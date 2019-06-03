903befd736a97266f7cd6a63f2aa9a3e17380600
Yanyinghenmei <1113135372@qq.om>
2019-06-04 00:05:14 +0800

    /api/register

--- www/handlers.py
+++ www/handlers.py
@@ -46,6 +46,7 @@ async def api_get_users(page='1',page_size='10',**kw):
     return dict(page=p, users=users)
 
 
+# ============= API =====================
 _RE_EMAIL = re.compile(r'^[a-z0-9\.\-\_]+\@[a-z0-9\-\_]+(\.[a-z0-9\-\_]+){1,4}$')
 _RE_SHA1 = re.compile(r'^[0-9a-f]{40}$')
 
@@ -62,20 +63,20 @@ async def signin():
     }
 
 @post('/api/register')
-def api_register(*, email, name, passwd):
+async def api_register(*, email, name, passwd):
     if not name or not name.strip():
-        raise apis.APIValueError('name')
+        raise APIValueError('name')
     if not email or not _RE_EMAIL.match(email):
-        raise apis.APIValueError('email')
+        raise APIValueError('email')
     if not passwd or not _RE_SHA1.match(passwd):
-        raise apis.APIValueError('passwd')
+        raise APIValueError('passwd')
     users = await User.findAll('email=?', [email])
     if len(users) > 0:
         raise apis.APIError('register: failed', 'email', 'Email is already in use.')
-    uid = next_id
+    uid = next_id()
     sha1_passwd = '%s:%s' % (uid, passwd)
     user = User(id=uid,name=name.strip(),email=email,passwd=hashlib.sha1(sha1_passwd.encode('utf-8')).hexdigest(),image='http://www.gravatar.com/avatar/%s?d=mm&s=120' % hashlib.md5(email.encode('utf-8')).hexdigest())
-    await user.save
+    await user.save()
     r =web.Response()
     r.set_cookie(COOKIE_NAME, user2cookie(user,86400), max_age=86400, httponly=True)
     user.passwd = '******'
@@ -83,11 +84,17 @@ def api_register(*, email, name, passwd):
     r.body = json.dumps(user, ensure_ascii=False).encode('utf-8')
     return r
 
+
+
 @post('/api/signin')
 def api_signin():
     pass
     
     
+
+
+#  ================= TOOLS ===================
+
 def get_page_index(page_str):
     p = 1
     try:
@@ -98,39 +105,42 @@ def get_page_index(page_str):
         p = 1
     return p
 
-
-
 def user2cookie(user, max_age):
-    '''
-    Generate cookie str by user.
-    '''
-    expires = str(int(time.time() + max_age))
-    s = '%s-%s-%s-%s' % (user.id, user.passwd, expires, _COOKIE_KEY)
-    L = [user.id, expires, hashlib.sha1(s.encode('utf-8')).hexdigest()]
-    return '-'.join(L)
+	'''
+	Generate cookie str by user.
+	'''
+	# build cookie string by: id-expires-sha1
+	expires = str(int(time.time() + max_age))
+	s = '%s-%s-%s-%s' % (user.id, user.passwd, expires, _COOKIE_KEY)
+	L = [user.id, expires, hashlib.sha1(s.encode('utf-8')).hexdigest()]
+	return '-'.join(L)
 
 async def cookie2user(cookie_str):
-    '''
-    Parse cookie and load user if collkie is valid
-    '''
-    if not cookie_str:
-        return None
-    try:
-        L = cookie_str.split('-')
-        if len(L) != 3:
-            return None
-        uid, expires, sha1 = L
-        if int(expires) < time.time():
-            return None
-        user = await User.find(uid)
-        if user is None:
-            return None
-        s = '%s-%s-%s-%s' % (uid, user.passwd, expires, _COOKIE_KEY)
-        if sha1 != hashlib.sha1(s.encode('utf-8')).hexdigest():
-            logging.info('invalid sha1')
-            return None
-        user.passwd = '******'
-        return user
-    except Exception as e:
-        logging.exception(e)
-        return None
+	'''
+	Parse cookie and load user if cookie is valid.
+	'''
+	if not cookie_str:
+		return None
+	try:
+		L = cookie_str.split('-')
+		if len(L) != 3:
+			return None
+		uid, expires, sha1 = L
+		if int(expires) < time.time():
+			return None
+		user = await User.find(uid)
+		if user is None:
+			return None
+		s = '%s-%s-%s-%s' % (uid, user.passwd, expires, _COOKIE_KEY)
+		if sha1 != hashlib.sha1(s.encode('utf-8')).hexdigest():
+			logging.info('invalid sha1')
+			return None
+		user.passwd = '******'
+		return user
+	except Exception as e:
+		logging.exception(e)
+		return None
+
+def text2html(text):
+	lines = map(lambda s: '<p>%s</p>' % s.replace('&', '&amp;').replace('<', '&lt;').replace('>', '&gt;'), filter(lambda s: s.strip() != '', text.split('\n')))
+	return ''.join(lines)
